d653a599683dd5dc5bb4b364233542409a5e13ed
Leo Bons <leobons@Leos-MacBook-Pro.local>
2025-10-14 15:07:25 +1300

    Build config + deploy script

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
+// next.config.ts
+import path from "path";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  outputFileTracingRoot: path.join(__dirname),
 };
 
 export default nextConfig;
